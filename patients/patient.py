6bbc25ea4485c38a6360267103bab1f0ce0d9b3f
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-27 02:16:40 -0800

    Email check

--- patients/patient.py
+++ patients/patient.py
@@ -12,15 +12,8 @@ class Patient:
         self.loggedIn = 1
         self.connection = sql.connect('patient.db')
         self.c = self.connection.cursor()
-        self.update()
-
-    def update(self):
         self.c.execute("INSERT INTO PatientDetail VALUES (null,?,?,?,?,?)",
                        (self.firstName, self.lastName, self.email, self.password, self.loggedIn))
-        self.c.execute("SELECT * FROM PatientDetail WHERE firstName =? AND lastName =? AND email =?", [self.firstName, self.lastName, self.email])
-        patientDetail = self.c.fetchall()
-        for i in patientDetail:
-            print(i)
 
     def login(self):
         email = input("Please enter your email. ")
@@ -31,12 +24,17 @@ class Patient:
         hash = ""
         for i in self.password:
             hash += "*"
-        print("Welcome, " + self.firstName +
-              "! Thank you for registering with UCH.")
-        print("First Name: " + self.firstName)
-        print("Last Name: " + self.lastName)
-        print("Email: " + self.email)
-        print("Password: " + hash)
+        self.c.execute("SELECT * FROM PatientDetail WHERE firstName =? AND lastName =? AND email =?",
+                       [self.firstName, self.lastName, self.email])
+        patientDetail = self.c.fetchall()
+        for i in patientDetail:
+            print("Welcome, " + i[1] + "! Thank you for registering with UCH.")
+            print("Patient ID: " + str(i[0]))
+            print("First Name: " + i[1])
+            print("Last Name: " + i[2])
+            print("Email: " + i[3])
+            print("Password: " + hash)
+            self.connection.commit()
 
     def loginCheck(self, email, password):
         if self.email == email and self.password == password:
@@ -51,10 +49,10 @@ class Patient:
         print("What would you like to do next?")
         print("Choose [1] to book an appointment")
         print("Choose [2] to cancel an appointment")
-        action=int(input("Choice: "))
+        action = int(input("Choice: "))
 
     def patientSummary(self):
-        hash=""
+        hash = ""
         for i in self.password:
             hash += "*"
         print("First Name: " + self.firstName)
@@ -64,25 +62,3 @@ class Patient:
         print("Questionnaire: " + str(self.questionnaire))
         print("Appointments: " + str(self.appointments))
         print("Prescriptions: " + str(self.prescriptions))
-
-
-def task():
-    print("Welcome!")
-    print("Choose [1] to register for a new account")
-    print("Choose [2] to login")
-    action=int(input("Choice: "))
-    if action == 1:
-        firstName=input("Please enter your first name. ")
-        lastName=input("Please enter your last name. ")
-        email=input("Please enter your email. ")
-        password=getpass("Please enter your password. ")
-        x=Patient(firstName, lastName, email, password)
-        x.registrationSummary()
-    elif action == 2:
-        email=input("Please enter your email. ")
-        password=getpass("Please enter your password. ")
-    else:
-        print("I'm sorry, that is an invalid option. Please type 'Register' or 'Login'. ")
-        task()
-
-task()
---
+++ patients/task.py
@@ -0,0 +1,39 @@
+import sqlite3 as sql
+from getpass import getpass
+from patient import Patient
+
+connection = sql.connect('patient.db')
+c = connection.cursor()
+
+def emailCheck(email):
+    c.execute("SELECT * FROM PatientDetail WHERE email =?", [email])
+    emails = c.fetchall()
+    if emails != []:
+        print("I'm sorry, that email is already in use. Please use another email.")
+        email=input("Please enter your email. ")
+        emailCheck(email)
+    else:
+        email = email
+
+def task():
+    print("Welcome!")
+    print("Choose [1] to register for a new account")
+    print("Choose [2] to login")
+    action=int(input("Choice: "))
+    if action == 1:
+        firstName=input("Please enter your first name. ")
+        lastName=input("Please enter your last name. ")
+        email=input("Please enter your email. ")
+        emailCheck(email)
+        password=getpass("Please enter your password. ")
+        x=Patient(firstName, lastName, email, password)
+        x.registrationSummary()
+        x.options()
+    elif action == 2:
+        email=input("Please enter your email. ")
+        password=getpass("Please enter your password. ")
+    else:
+        print("I'm sorry, that is an invalid option. Please type 'Register' or 'Login'. ")
+        task()
+
+task()
