41e7d2ac135114138b0866181ad331acda614cc1
Erin Kwon <wei.quan.20@ucl.ac.uk>
2020-12-01 01:03:46 +0000

    data insertion issue fixed

--- patients/PatientRiskProfile.py
+++ patients/PatientRiskProfile.py
@@ -14,7 +14,7 @@ class PatientMedical:
         count = 0
         answers_to_vac = []
         for name in self.vaccination_history:
-            vaccine = input('Have you had the {} vac cination: '.format(name)).lower()
+            vaccine = input('Have you had the {} vaccination: '.format(name)).lower()
             answers_to_vac.append(vaccine)
             if vaccine == "no":
                 print("Please book an appointment with your GP to receive your {} vaccination "
@@ -30,12 +30,12 @@ class PatientMedical:
         show = self.a.fetchall()
         print(show)
 
-    def cancer(self):
+    def cancer_family(self):
         print("Thank you! The following questions are concerned with the medical history of your family.")
-        print('Please enter your answers to the following questions with Yes/No')
+        print('Please enter your answers to the following questions with Y/N')
         cancer = input('Has anyone in your immediate family, parents or siblings, '
                        'ever been diagnosed with cancer: ').lower()
-        if cancer == "yes":
+        if cancer == "y":
             cancer_type = input("Please tell us the type(s) of cancer that was diagnosed: ").split(",")
             for cancer_name in cancer_type:
                 cancer_age = input('How old was the person diagnosed with {}: '.format(cancer_name))
@@ -44,6 +44,17 @@ class PatientMedical:
         else:
             print("Wonderful! That means you do not likely have any genetic risk in any specific cancer that we know "
                   "so far based on your family medical history")
+
+    # def cancer(self):
+    #     print("Thank you! The following questions are concerned with your medical history.")
+    #     print('Please enter your answers to the following questions with Y/N')
+    #     cancer = input('Have you ever been diagnosed with cancer: ').lower()
+    #     if cancer == "y":
+    #         cancer_type = input("Please tell us the type(s) of cancer that was diagnosed: ").split(",")
+    #         for cancer_name in cancer_type:
+    #             cancer_age = input('How old were you when you were diagnosed with {}: '.format(cancer_name))
+    #             self.cancer_history[cancer_name] = cancer_age
+    #         print(self.cancer_history)
 
         self.connection.commit()
         self.connection.close()
--- patients/questionnaireTable.py
+++ patients/questionnaireTable.py
@@ -4,8 +4,8 @@ conn = sql.connect('questionnaireTable.db')
 
 c = conn.cursor()
 
-# c.execute('DROP TABLE IF EXISTS questionnaireTable')
-c.execute("""CREATE TABLE questionnaireTable(
+c.execute('DROP TABLE IF EXISTS questionnaireTable')
+c.execute("""CREATE TABLE IF NOT EXISTS questionnaireTable(
             PatientID INTEGER PRIMARY KEY,
             exercise DATATYPE text,
             exerciseType DATATYPE text,
@@ -13,12 +13,16 @@ c.execute("""CREATE TABLE questionnaireTable(
             exerciseDuration DATATYPE integer,
             goal DATATYPE text,
             height DATATYPE real,
-            weight DATATYPE real
+            weight DATATYPE real,
             bmi DATATYPE real,
             smoking DATATYPE text,
             drugs DATATYPE text,
             drugType DATATYPE text,
-            alcoholUnit DATATYPE text
+            alcohol DATATYPE text,
+            alcoholUnit DATATYPE text,
+            meat DATATYPE text,
+            diet DATATYPE text,
+            caffeine DATATYPE text
             )""")
 conn.commit()
 conn.close()
