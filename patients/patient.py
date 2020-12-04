d76d4e35ce33fa09144b36da70e06b1c8f97abfe
AriannaBourke <ariannabourke@hotmail.com>
2020-12-04 09:15:03 +0000

    appointment changes

--- patients/patient.py
+++ patients/patient.py
@@ -54,25 +54,41 @@ class Patient:
 
     def appointmentCalendar(self):
         global gpLastName
+        global date
 
+        print("**********"
+              "\n [1] January     \t[2] February      \t[3] March"
+              "\n [4] April     \t\t[5] May           \t[6] June"
+              "\n [7] July      \t\t[8] August        \t[9] September "
+              "\n [10] October    \t[11] November     \t[12] December")
+        mm = int(input("Please choose the month would you would like your appointment in 2021: "))
         print("----------")
         print(calendar.month(2021, 1))
         print("----------")
-        day = int(input("Please select a day in January: "))
+        day = int(input("Please select a day: "))
         # amend error handling
         if day <= 0 or day > 31:
             day = int(input("This is not a valid day."
-                            "\nPlease select a day in January: "))
-        date = "{:0>2}/01/2021".format(day)
+                            "\nPlease select a day: "))
+        date = "{:0>2}/{:0>2}/2021".format(day, mm)
 
-        print()
-        print("This is the current availability for Dr {} on your chosen date: ".format(gpLastName))
-        self.c.execute("SELECT time, bookedStatus FROM Appointment WHERE date =? and gpLastName =? "
-                       "and gpAvailability != 'unavailable'",
-                       [date, gpLastName])
+        print("\nThis is the current availability for Dr {} on your chosen date: ".format(gpLastName))
+        self.c.execute("SELECT time, bookedStatus FROM Appointment WHERE date =? and gpLastName =?", [date, gpLastName])
         appointments = self.c.fetchall()
-        for app in appointments:
-            print(app[0] + "\t\t" + app[1])
+        times = ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
+                 "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"]
+        for time in times:
+            for app in appointments:
+                time1 = (app[0])
+                status = (app[1])
+                if time1 == time:
+                    print(time1 + " " + status)
+                else:
+                    print(time + " available")
+        self.chooseTime()
+
+    def chooseTime(self):
+        global date
 
         print("**********"
               "\n[1] to select a time"
@@ -83,21 +99,21 @@ class Patient:
         if options == 1:
             time = input("Please choose a time from the available appointments: ")
             # add in error handling
-            chosen = [self.patientEmail, time, date, gpLastName]
-            self.c.execute("""UPDATE Appointment SET bookedStatus = 'Booked', patientEmail =?
-                            WHERE time =? and date=? and gpLastName =?""", chosen)
+            bookedStatus = 'Pending'
+            checkIn = 'null'
+            checkOut = 'null'
+            self.c.execute("INSERT INTO Appointment VALUES (null,?,?,?,?,?,?,?)",
+                           (self.email, date, time, gpLastName, bookedStatus, checkIn, checkOut))
             self.connection.commit()
             print("You have requested to book an appointment on {} at {}, "
-                  "you will receive confirmation of your appointment shortly.".format(date, time))
+                  "\nyou will receive confirmation of your appointment shortly.".format(date, time))
             if input("Type yes to return to the appointment menu: ").lower() == 'yes':
                 self.bookAppointment()
 
         if options == 2:
             self.appointmentCalendar()
-
         if options == 3:
-            self.bookAppointment()
-            pass
+            self.returnToAppMenu()
 
     def chooseDoctor(self):
         global gpLastName
@@ -110,76 +126,85 @@ class Patient:
               "\n**********")
         dr_options = int(input("Please choose from the options above: "))
 
-        # select specific dr
         if dr_options == 1:
-            print("**********"
-                  "\nThe doctors currently available at the practice are: ")
-            self.c.execute("SELECT DISTINCT(gpLastName) FROM Appointment")
-            dr_names = self.c.fetchall()
-            gp_list = []
-            for dr in dr_names:
-                print("Dr", dr[0])
-                gp_list.append(dr)
-            dr_option1 = input("**********"
-                               "\nPlease enter the name of the doctor you would "
-                               "like to book an appointment with: ")
-            # if dr_option1 not in gp_list:
-            #     print("This is not a valid choice of doctor, please try again")
-            gpLastName = dr_option1
-            self.appointmentCalendar()
-
-        # random doctor
+            self.chooseSpecificDr()
         if dr_options == 2:
-            self.c.execute("SELECT DISTINCT(gpLastName) FROM Appointment")
-            dr_names = self.c.fetchall()
-            gp_list = []
-            for dr in dr_names:
-                gp_list.append(dr[0])
-
-            gpLastName = random.choice(gp_list)
-            print("The doctor you have been assigned is Dr {}".format(gpLastName))
-            self.appointmentCalendar()
-
-        # doctor of specific gender
+            self.chooseAnyDr()
         if dr_options == 3:
-            print("**********"
-                  "\n[1] To book an appointment with a male doctor"
-                  "\n[2] to book an appointment with a female doctor")
-            gp_options = int(input("Please choose from the options above: "))
-
-            if gp_options == 1:
-                self.c.execute("SELECT gpLastName FROM GP WHERE gpGender = 'M'")
-                dr_names = self.c.fetchall()
-                gp_list = []
-                for dr in dr_names:
-                    gp_list.append(dr[0])
-
-                gpLastName = random.choice(gp_list)
-                print()
-                print("The male doctor you have been assigned is Dr {}".format(gpLastName))
-                self.appointmentCalendar()
-
-            if gp_options == 2:
-                self.c.execute("SELECT gpLastName FROM GP WHERE gpGender = 'F' ")
-                dr_names = self.c.fetchall()
-                gp_list = []
-                for dr in dr_names:
-                    gp_list.append(dr[0])
-
-                gpLastName = random.choice(gp_list)
-                print(gpLastName)
-                print("The female doctor you have been assigned is Dr {}".format(gpLastName))
-                self.appointmentCalendar()
-
+            self.chooseDrGender()
         if dr_options == 4:
-            self.bookAppointment()
-            pass
+            self.returnToAppMenu()
+
+    def chooseSpecificDr(self):
+        global gpLastName
+        print("**********"
+              "\nThe doctors currently available at the practice are: ")
+        self.c.execute("SELECT DISTINCT(gpLastName) FROM GP")
+        dr_names = self.c.fetchall()
+        gp_list = []
+        for dr in dr_names:
+            print("Dr", dr[0])
+            gp_list.append(dr)
+        dr_option1 = input("**********"
+                           "\nPlease enter the name of the doctor you would "
+                           "like to book an appointment with: ")
+        # if dr_option1 not in gp_list:
+        #     print("This is not a valid choice of doctor, please try again")
+        gpLastName = dr_option1
+        self.appointmentCalendar()
+
+    def chooseAnyDr(self):
+        global gpLastName
+        self.c.execute("SELECT DISTINCT(gpLastName) FROM GP")
+        dr_names = self.c.fetchall()
+        gp_list = []
+        for dr in dr_names:
+            gp_list.append(dr[0])
+
+        gpLastName = random.choice(gp_list)
+        print("The doctor you have been assigned is Dr {}".format(gpLastName))
+        self.appointmentCalendar()
+
+    def chooseDrGender(self):
+        print("**********"
+              "\n[1] To book an appointment with a male doctor"
+              "\n[2] to book an appointment with a female doctor")
+        gp_options = int(input("Please choose from the options above: "))
+
+        if gp_options == 1:
+            self.chooseMDr()
+        if gp_options == 2:
+            self.chooseFDr()
+
+    def chooseMDr(self):
+        global gpLastName
+        self.c.execute("SELECT gpLastName FROM GP WHERE gpGender = 'M'")
+        dr_names = self.c.fetchall()
+        gp_list = []
+        for dr in dr_names:
+            gp_list.append(dr[0])
+
+        gpLastName = random.choice(gp_list)
+        print("\nThe male doctor you have been assigned is Dr {}".format(gpLastName))
+        self.appointmentCalendar()
+
+    def chooseFDr(self):
+        global gpLastName
+        self.c.execute("SELECT gpLastName FROM GP WHERE gpGender = 'F' ")
+        dr_names = self.c.fetchall()
+        gp_list = []
+        for dr in dr_names:
+            gp_list.append(dr[0])
+
+        gpLastName = random.choice(gp_list)
+        print("\nThe female doctor you have been assigned is Dr {}".format(gpLastName))
+        self.appointmentCalendar()
 
     def cancelAppointment(self):
         print("**********"
               "\nThese are your booked appointments: ")
         self.c.execute("SELECT appointmentID, date, time, gpLastName FROM Appointment WHERE patientEmail =?",
-                       [self.patientEmail])
+                       [self.email])
         appointments = self.c.fetchall()
         for app in appointments:
             print("Appointment ID: " + str(app[0]) + "\t" + "date: " + app[1] + "\t"
@@ -187,51 +212,37 @@ class Patient:
 
         print("**********"
               "\n[1] To cancel an appointment"
-              "\n[2] To exit to the main menu"
+              "\n[2] To exit to the appointment menu"
               "\n**********")
         options = int(input("Please choose from the options above: "))
 
-        print()
         if options == 1:
-            cancel = input("Please enter the appointment ID you would like to cancel: ")
-            self.c.execute("UPDATE Appointment SET bookedStatus = 'Available', patientEmail = '' "
-                           "WHERE appointmentID =?", [cancel])
+            cancel = input("\nPlease enter the appointment ID you would like to cancel: ")
+            self.c.execute("DELETE FROM Appointment WHERE appointmentID =?", [cancel])
             self.connection.commit()
             print("You have cancelled your appointment")
-            if input("Type yes to return to the appointment menu: ").lower() == 'yes':
-                self.bookAppointment()
+            self.returnToAppMenu()
+
         if options == 2:
-            self.bookAppointment()
+            self.returnToAppMenu()
 
     def viewAppConfirmations(self):
         print("**********"
               "\nThese are your booked appointments: ")
         self.c.execute("SELECT appointmentID, date, time, gpLastName FROM Appointment WHERE patientEmail =?",
-                       [self.patientEmail])
+                       [self.email])
         appointments = self.c.fetchall()
+
         for app in appointments:
             print("Appointment ID: " + str(app[0]) + "\t" + "date: " + app[1] + "\t"
                   + "time: " + app[2] + "\t\t" + "with: Dr " + app[3])
+        self.returnToAppMenu()
+
+    def returnToAppMenu(self):
         if input("Type yes to return to the appointment menu: ").lower() == 'yes':
             self.bookAppointment()
 
-    # not a real function, just sets one appointment to unavailable for testing
-    def checkDrAvailable(self):
-        self.c.execute("""UPDATE Appointment SET drAvailability = 'unavailable'
-                        WHERE time ='11:30' and date='01/01/2021' and gpLastName ='Shepherd'""")
-        self.connection.commit()
-        pass
+ari = Patient("Arianna", "Bourke", "ariannabourke@hotmail.com", "1234")
+ari.bookAppointment()
 
-
-
-# ari = Patient(1, "Arianna", "Bourke", "ariannabourke@hotmail.com", "1234")
-# ari.bookAppointment()
-# ari.cancelAppointment()
-# ari.viewAppConfirmations()
-# ari.checkDrAvailable()
-
-# conn = sql.connect('patient.db')
-# c = conn.cursor()
-# c.execute("SELECT * FROM Appointment WHERE patientEmail = '1'")
-# print(c.fetchall())
 
--- patients/patientData.py
+++ patients/patientData.py
@@ -50,8 +50,8 @@ c.execute(""" CREATE TABLE IF NOT EXISTS GP (
                 gpGender DATATYPE text
                 )""")
 
-c.execute("INSERT INTO GP VALUES ('Shep@erd.com', 'Shepherd', 'M')")
-c.execute("INSERT INTO GP VALUES ('Meridith@grey.com', 'Grey', 'F' )")
+c.execute("INSERT INTO GP VALUES ('Shepherd@dr.com', 'Shepherd', 'M')")
+c.execute("INSERT INTO GP VALUES ('Grey@dr.com', 'Grey', 'F' )")
 c.execute("INSERT INTO GP VALUES ('Bailey@dr.com', 'Bailey', 'F' )")
 
 c.execute(""" CREATE TABLE IF NOT EXISTS Appointment (
@@ -61,36 +61,9 @@ c.execute(""" CREATE TABLE IF NOT EXISTS Appointment (
                 time DATATYPE text,
                 gpLastName DATATYPE text,
                 bookedStatus DATATYPE text,
-                gpAvailability DATATYPE text,
                 checkIn DATATYPE text DEFAULT NULL,
                 checkOut DATATYPE text DEFAULT NULL
                 )""")
-
-times = ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30',
-        '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30',
-        '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30'
-         ]
-
-dates = ['01/01/2021', '02/01/2021', '03/01/2021', '04/01/2021', '05/01/2021', '06/01/2021',
-         '07/01/2021', '08/01/2021', '09/01/2021', '10/01/2021', '11/01/2021', '12/01/2021',
-         '13/01/2021', '14/01/2021', '15/01/2021', '16/01/2021', '17/01/2021', '18/01/2021',
-         '19/01/2021', '20/01/2021', '21/01/2021', '22/01/2021', '23/01/2021', '24/01/2021',
-         '25/01/2021', '26/01/2021', '27/01/2021', '28/01/2021', '29/01/2021', '30/01/2021',
-         '31/01/2021'
-         ]
-
-gp_list = ['Shepherd', 'Grey', 'Bailey']
-
-patientEmail = ''
-bookedStatus = 'Available'
-gpAvailability = 'Available'
-
-for date in dates:
-    for time in times:
-        for gpLastName in gp_list:
-            c.execute("INSERT INTO Appointment VALUES (null, ?, ?, ?, ?, ?, ?, null, null)", (patientEmail, date, time,
-                                                                                            gpLastName, bookedStatus,
-                                                                                           gpAvailability))
 
 conn.commit()
 conn.close()
