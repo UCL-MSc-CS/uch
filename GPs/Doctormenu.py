96c62da62bada9eec16b221a1dbc1a17261bae61
ucabcnd <73539195+ucabcnd@users.noreply.github.com>
2020-12-05 13:53:11 +0000

    added mainmenu function

--- GPs/Doctormenu.py
+++ GPs/Doctormenu.py
@@ -17,29 +17,30 @@ def continueorexit():
         else:
             print("Please enter a valid response")
 
-doctoremail = "drgrey@gmail.com"
-continueloop = [True]
+def mainmenu(drdemail):
+    doctoremail = drdemail
+    continueloop = [True]
 
-#Todo get doctor details from query for the below print statement
-print("Welcome Doctor " + doctoremail)
-while continueloop[0]:
-    print("choose [1] to open appointment")
-    print("choose [2] to confirm appointments")
-    print("choose [3] to add non-patient time (including holidays)")
-    print("choose [4] to view your timetable")
-    print("choose [5] to cancel appointments")
-    option = input(":")
-    if option == "1":
-        openappointment(doctoremail)
-    elif option == "2":
-        confirmappointments(doctoremail)
-    elif option == "3":
-        addnonpatienttime(doctoremail)
-    elif option == "4":
-        printtimetable(doctoremail)
-    elif option == "5":
-        clearhours(doctoremail)
-    else:
-        print("Invalid option chosen. Try again")
-    continueorexit()
-    print("------------------------------------------------------------------------------------------------------")
+    #Todo get doctor details from query for the below print statement
+    print("Welcome Doctor " + doctoremail)
+    while continueloop[0]:
+        print("choose [1] to open appointment")
+        print("choose [2] to confirm appointments")
+        print("choose [3] to add non-patient time (including holidays)")
+        print("choose [4] to view your timetable")
+        print("choose [5] to cancel appointments")
+        option = input(":")
+        if option == "1":
+            openappointment(doctoremail)
+        elif option == "2":
+            confirmappointments(doctoremail)
+        elif option == "3":
+            addnonpatienttime(doctoremail)
+        elif option == "4":
+            printtimetable(doctoremail)
+        elif option == "5":
+            clearhours(doctoremail)
+        else:
+            print("Invalid option chosen. Try again")
+        continueorexit()
+        print("------------------------------------------------------------------------------------------------------")
