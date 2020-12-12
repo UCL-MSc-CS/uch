2f04395f917cbc5e2c95e750d987fe5cd3ae994a
Caroline Crandell <carolineecrandell@gmail.com>
2020-12-12 03:39:38 -0800

    Added NHSNumber

--- patients/patient.py
+++ patients/patient.py
@@ -1,10 +1,12 @@
 import sqlite3 as sql
-from getpass import getpass
+import random
+
 
 class Patient:
 
     def __init__(self, patientEmail, firstName, lastName, dateOfBirth, age, gender, addressLine1, addressLine2,
                  postcode, telephoneNumber, password):
+        self.nhsNumber = 0
         self.patientEmail = patientEmail
         self.firstName = firstName
         self.lastName = lastName
@@ -16,17 +18,45 @@ class Patient:
         self.postcode = postcode
         self.telephoneNumber = telephoneNumber
         self.password = password
-        self.loggedIn = 1
         self.registrationConfirm = 0
         self.connection = sql.connect('UCH.db')
         self.c = self.connection.cursor()
+        self.nhsGenerator()
 
     def register(self):
-        self.c.execute("INSERT INTO PatientDetail VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",
-                       (self.patientEmail, self.firstName, self.lastName, self.dateOfBirth, self.age, self.gender,
+        self.c.execute("INSERT INTO PatientDetail VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                       (self.nhsNumber, self.patientEmail, self.firstName, self.lastName, self.dateOfBirth, self.age, self.gender,
                         self.addressLine1, self.addressLine2, self.postcode, self.telephoneNumber, self.password,
-                        self.loggedIn, self.registrationConfirm))
+                        self.registrationConfirm))
         self.connection.commit()
+    
+    def nhsGenerator(self):
+        count = 0
+        nhsNumber = ""
+        while count < 10:
+            x = random.randint(0,9)
+            nhsNumber += str(x)
+            count += 1
+        # self.c.execute("SELECT * FROM PatientDetail WHERE nhsNumber =?", [nhsNumber])
+        # nhsNumbers = self.c.fetchall()
+        # if nhsNumbers != []:
+        #     while nhsNumbers != []:
+        #         count = 0
+        #         nhsNumber = ""
+        #         while count < 10:
+        #             x = random.randint(0,9)
+        #             nhsNumber += str(x)
+        #             count += 1
+        #         self.c.execute("SELECT * FROM PatientDetail WHERE nhsNumber =?", [nhsNumber])
+        #         nhsNumbers = self.c.fetchall()
+        self.nhsNumber = nhsNumber
+    
+    def printNHS(self):
+        x = str(self.nhsNumber)
+        one = x[0:3]
+        two = x[3:6]
+        three = x[6:10]
+        print(one, two, three)
 
     def registrationSummary(self):
         hash = ""
@@ -43,12 +73,6 @@ class Patient:
             print("Password: " + hash)
             self.connection.commit()
 
-
-# ari = Patient("ariannabourke@hotmail.com", "Arianna", "Bourke", "27/04/1988", 10, "male",
-#               "123 Happy", "street", "12343", "389753957", "1234")
-
-
-
-
-
-
+kamala = Patient("hello@me.com", "Kamala", "Harris", "27/04/1988", 10, "male",
+              "123 Happy", "street", "12343", "389753957", "1234")
+kamala.printNHS()
