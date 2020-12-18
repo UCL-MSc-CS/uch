0a269dfaaf598f72d22142f7964b4471d11558d7
Post Shalom <jacobglapkin@gmail.com>
2020-12-18 15:29:17 +0000

    Update Admins.py
    added only time message not date

--- Admins.py
+++ Admins.py
@@ -508,7 +508,7 @@ class adminFunctions():
                 firstsel = self.c.fetchall()
                 self.c.execute("""UPDATE Appointment SET checkIn = ? WHERE appointmentID = ? """, (unixd, check_number))
                 self.connection.commit()
-                x = dt.now()
+                x = dt.now().time()
                 print("Successfully checked in {} at {a}".format(firstsel[0][0], a=x))
                 return 0
 
@@ -547,7 +547,7 @@ class adminFunctions():
                 firstsel = self.c.fetchall()
                 self.c.execute("""UPDATE Appointment SET checkOut = ? WHERE appointmentID = ? """, (unixd, check_number))
                 self.connection.commit()
-                x = dt.now()
+                x = dt.now().time()
                 print("Successfully checked out {} at {a}".format(firstsel[0][0], a = x))
                 return 0
 
